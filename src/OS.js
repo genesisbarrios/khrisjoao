7d73ffe03676af6f26611b9c430ad13ba9650a27
Genesis Barrios <genesisbarriosdev@gmail.com>
2023-12-31 06:49:43 -0500

    added youtube and more nfts

--- src/OS.js
+++ src/OS.js
@@ -69,7 +69,7 @@ const GenWavOS = (props) => {
           </div>
           <div style={{marginTop:"10%", marginLeft:"5%"}}>
             <ThemeProvider theme={brick}>
-                <Window style={{width:'500px', minHeight: '700px'}}>
+                <Window style={{width:'80%', minHeight: '700px'}}>
                   <WindowHeader><img src="https://drive.google.com/uc?export=view&id=1Uvtq5e8SooDABw2cRESMMeLLcYc_tee3" width="20" height="20"/> Beats</WindowHeader>
                   <WindowContent>
                     <iframe 
@@ -80,11 +80,19 @@ const GenWavOS = (props) => {
                   </WindowContent>
                 </Window>
               </ThemeProvider>
+              <ThemeProvider theme={brick}>
+                <Window style={{width:'80%', minHeight: '350px', marginTop:"3%"}}>
+                  <WindowHeader> Original Music by gen.wav </WindowHeader>
+                  <WindowContent>
+                  <iframe width="100%" height="315" src="https://www.youtube.com/embed/videoseries?si=xGEY6ZggMM4ptxno&amp;list=PLcGBMxEyx5p8fc_zHMGJAUxLs94iw_puY" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfulscreen></iframe>
+                   </WindowContent>
+                </Window>
+              </ThemeProvider>
           </div>
         </Grid>
         <Grid item sm={4}>
           <ThemeProvider theme={blue}>
-            <Window style={{width:'500px', minHeight: '200px'}}>
+            <Window style={{width:'80%', minHeight: '200px', marginTop:'20%'}}>
               <WindowHeader> About</WindowHeader>
               <WindowContent>
               <p> Hi, my name is gen.wav. I'm a Cuban-American Artist, Producer, and Software Engineer. 👨‍💻 I like to say I'm a creative because aside from programming, and making music, I do photography, I dabble in videography and graphic design, I do live coding, I DJ, and I make YouTube videos. So, take a look around, you can stream my music, watch my music videos, buy beats, or Music NFTS, or just vibe.</p>
@@ -94,7 +102,7 @@ const GenWavOS = (props) => {
         </Grid>
         <Grid item sm={4}>
          <ThemeProvider theme={blue}>
-            <Window style={{width:'500px', minHeight: '200px'}}>
+            <Window style={{width:'80%', minHeight: '200px', marginTop:'20%'}}>
               <WindowHeader>🎤 Stream My Music 🎤</WindowHeader>
               <WindowContent>
               {/* <iframe width="100%" height="166" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/genesisbarrios/sets/my-music?si=62d1c9cdb8d54729a394f3c43d5d8d1b"></iframe>
@@ -112,18 +120,26 @@ const GenWavOS = (props) => {
           </ThemeProvider>
 
           <ThemeProvider theme={blue}>
-              <Window style={{width:'500px', minHeight: '800px', marginTop:'20px'}}>
+              <Window style={{width:'80%', minHeight: '800px', marginTop:'20px'}}>
                 <WindowHeader>💿 Music NFTs</WindowHeader>
                 <WindowContent>
                   <Frame style={{marginBottom:"10px"}}>
                     For those that don't know about Music NFTs, they are digital collectibles. You can collect(or mint) to own a rare, and unique copy of one of my songs. These collectibles will give you access to different features on my website.. It may be free beats, sample packs, or sneak peeks!
                   </Frame>
                   <Frame style={{marginBottom:"10px", width:"100%"}}>
-                  <div><iframe id="embed" width="100%" height="700px" src="https://zora.co/editions/eth:0x5bc30e809aed2ff3c3bede9804419488e305b258/frame?padding=20px&mediaPadding=20px&showDetails=false&theme=%7B%22foreground%22%3A%22%23000000%22%2C%22background%22%3A%22%23f3f3f3%22%2C%22accent%22%3A%22%2307ff3b%22%2C%22onAccent%22%3A%22%23000000%22%2C%22border%22%3A%22%23f3f3f3%22%2C%22background2%22%3A%22%23f3f3f3%22%2C%22positive%22%3A%22%231CB687%22%2C%22negative%22%3A%22%23F03232%22%2C%22warning%22%3A%22%23F5A623%22%7D&showMedia=true&showCollectors=false&showMintingUI=true"></iframe></div>
+                  <div><iframe id="embed" width="100%" height="700px" src="https://zora.co/editions/eth:0x5bc30e809aed2ff3c3bede9804419488e305b258/1/frame?padding=20px&mediaPadding=20px&showDetails=false&theme=%7B%22foreground%22%3A%22%23000000%22%2C%22background%22%3A%22%23f3f3f3%22%2C%22accent%22%3A%22%2307ff3b%22%2C%22onAccent%22%3A%22%23000000%22%2C%22border%22%3A%22%23f3f3f3%22%2C%22background2%22%3A%22%23f3f3f3%22%2C%22positive%22%3A%22%231CB687%22%2C%22negative%22%3A%22%23F03232%22%2C%22warning%22%3A%22%23F5A623%22%7D&showMedia=true&showCollectors=false&showMintingUI=true"></iframe></div>
                   </Frame>
+                  <Frame style={{marginBottom:"10px", width:"100%"}}>
+                  <div><iframe id="embed" width="100%" height="700px" src="https://zora.co/collect/oeth:0xb45cd602810c48df0e36c1c292c51b4dd21f8e9d/1/frame?padding=20px&mediaPadding=20px&showDetails=false&theme=%7B%22foreground%22%3A%22%23000000%22%2C%22background%22%3A%22%23f3f3f3%22%2C%22accent%22%3A%22%2307ff3b%22%2C%22onAccent%22%3A%22%23000000%22%2C%22border%22%3A%22%23f3f3f3%22%2C%22background2%22%3A%22%23f3f3f3%22%2C%22positive%22%3A%22%231CB687%22%2C%22negative%22%3A%22%23F03232%22%2C%22warning%22%3A%22%23F5A623%22%7D&showMedia=true&showCollectors=false&showMintingUI=true"></iframe></div>
+                  </Frame>
+                  <Frame style={{marginBottom:"10px", width:"100%"}}>
+                  <div><iframe id="embed" width="100%" height="700px" src="https://zora.co/collect/oeth:0xe5a213495a6186bc85b6411c35286a858cb3675f/1/frame?padding=20px&mediaPadding=20px&showDetails=false&theme=%7B%22foreground%22%3A%22%23000000%22%2C%22background%22%3A%22%23f3f3f3%22%2C%22accent%22%3A%22%2307ff3b%22%2C%22onAccent%22%3A%22%23000000%22%2C%22border%22%3A%22%23f3f3f3%22%2C%22background2%22%3A%22%23f3f3f3%22%2C%22positive%22%3A%22%231CB687%22%2C%22negative%22%3A%22%23F03232%22%2C%22warning%22%3A%22%23F5A623%22%7D&showMedia=true&showCollectors=false&showMintingUI=true"></iframe></div>
+                  </Frame>
+
                   <Frame style={{marginBottom:"10px"}}>
                     <Button><a href="https://beta.catalog.works/gendotwav/soltero" target="_blank" style={{textDecoration:"none"}}>Soltero feat. El Igor on Catalog</a></Button>
                   </Frame>
+                  <br></br>
                   <Frame style={{marginBottom:"10px"}}>
                     <Button><a href="https://www.riffapp.xyz/post/0x01a1b9-0x12" target="_blank" style={{textDecoration:"none"}}>La Playa Snippet - Free on Riffapp</a></Button>
                   </Frame>
