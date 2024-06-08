bb8b70fa9874de8c7f14a3a8107d6e3b03bf168f
Genesis Barrios <genesisbarriosdev@gmail.com>
2024-06-08 01:05:56 -0400

    corazon frio

--- src/GENESIS.js
+++ src/GENESIS.js
@@ -197,6 +197,9 @@ const EPK = (props) => {
           <Grid item xs={12} lg={4}>
             <iframe width="100%" height="315" src="https://www.youtube.com/embed/bPX7_MUQr-0?si=zJLxLxCxsVCqgNr7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </Grid>
+          <Grid item xs={12} lg={4}>
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/3wBxgLcn4-M?si=VxC900P3cLHoeUIt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </Grid>
       </Grid>
       
       <div className="aboutAlbum">
