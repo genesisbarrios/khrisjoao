908624c781d00903fbaf03c3eedc05b944d05d25
Genesis Barrios <genesisbarriosdev@gmail.com>
2024-10-29 02:29:49 -0400

    releases page sin destino

--- src/Header.js
+++ src/Header.js
@@ -17,7 +17,6 @@ const Header = () => {
         </Typography>
         <nav>
           <ul style={{ display: 'flex', listStyleType: 'none', margin: 0, padding: 0 }}>
-          <li style={{ marginRight: '10px'}}><Link style={{color:"white", textDecoration:"none" }} to="/LOSIGNORO">NEW</Link></li>
           <li style={{ marginRight: '10px'}}><Link style={{color:"white", textDecoration:"none" }}  to="/GENESIS">GENESIS</Link></li>
           <li style={{ marginRight: '10px'}}><Link style={{color:"white", textDecoration:"none" }} to="/RELEASES">RELEASES</Link></li>
           <li style={{ marginRight: '10px'}}><Link style={{color:"white", textDecoration:"none" }} to="/EPK">EPK</Link></li>
