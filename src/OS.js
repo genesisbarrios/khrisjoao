2e31ffaee1bcc3771f3bccc19dca3be3e2440d76
Genesis Barrios <genesisbarriosdev@gmail.com>
2024-10-29 01:35:41 -0400

    upate bio

--- src/OS.js
+++ src/OS.js
@@ -185,7 +185,7 @@ const GenWavOS = (props) => {
                     <div style={{textAlign:"center"}}> 
                       <Avatar size={125} src='https://dl.dropboxusercontent.com/s/aw5uvuev357azdlo72og2/Screenshot-2024-04-18-at-2.18.45-AM.png?rlkey=waj1wtalo8qvyjddhyf768jw0&dl=0' />
                     </div>
-                    <Paper> Hi, my name is gen.wav. I'm a Cuban-American Artist, Producer, Multi-Instrumentalist, Audio and Software Engineer. 👨‍💻 I'm a life-long musician, with a background in classical training, where I received years of training in Music Theory, Ear Training, and Composition, and learned many instruments, including Piano, Cello, Violin, and Voice. I'm a self taught guitarist, bassist, and producer, with over 10 years of experience writing, recording, producing and performing music. Take a look around at my work, you can stream my music, watch my music videos, buy beats, or Music NFTS, or just vibe. 🌊</Paper>
+                    <Paper> Hi, my name is gen.wav. I'm a Cuban-American Artist, Producer, Multi-Instrumentalist, Audio and Software Engineer. 👨‍💻 I'm a life-long musician, with a background in classical training, years of training in Music Theory, Ear Training, and Composition, and many instruments including Piano, Cello, and Voice. I'm a self taught Guitarist, Bassist, and Producer, with over 10 years of experience Composing, Producing, and Performing music. Take a look around at my custom coded site where you can stream my music, watch my music videos, buy beats, or Music NFTS, or just vibe. 🌊</Paper>
                   </CardContent>
               </Card>
           
