05ebe4d8b9c0a0328f8bc791cb333c19c4e5fc02
Genesis Barrios <genesisbarriosdev@gmail.com>
2025-12-02 03:23:12 -0500

    mini cd added

--- src/SOLYMAR.js
+++ src/SOLYMAR.js
@@ -225,7 +225,7 @@ const SOLYMAR = (props) => {
               }
             `}
            </style>
-            <h3 className="solymar-mini-h3" style={{marginTop:"2%", marginBottom:"2%"}}>SolyMar MINI CD NFC Keychain</h3>
+            <h3 className="solymar-mini-h3" style={{marginTop:"2%", marginBottom:"2%"}}>SolyMar MINI CD NFC Keychain 🔑🔗</h3>
            <img src="/SolyMar-BOX AND CD-square.jpg" width="30%" style={{borderRadius:"10px"}}></img>
             <h3 className="solymar-mini-h3" style={{marginTop:"2%"}}>Tap to download of all 13 tracks on the album 🎵</h3>
           </a>
